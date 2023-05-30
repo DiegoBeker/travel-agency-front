09191280f9420c20ffba9a394501f9552cf1aca8
Diego <diegobecker2007@hotmail.com>
2023-05-30 10:27:11 -0300

    feat: add dinamic hotels details

--- src/Pages/HotelsDetailsPage/HotelsDetailsPage.js
+++ src/Pages/HotelsDetailsPage/HotelsDetailsPage.js
@@ -1,9 +1,27 @@
 import { useParams } from "react-router-dom";
 import { AmenitiesContainer, Content, HotelContent, InfoContainer, PageContainer, PicturesContainer } from "./style";
 import Header from "../../components/Header/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function HotelsDetailsPage() {
     const { cityName, hotelId } = useParams();
+    const [info, setInfo] = useState(undefined);
+    console.log(info);
+
+    useEffect(()=>{
+        axios.get(`${process.env.REACT_APP_API_URL}/hotels/${cityName}/${hotelId}`)
+            .then((response)=> {
+                setInfo(response.data);
+            })
+            .catch((err) => console.log(err));
+    },[]);
+
+    if(info === undefined) return(
+        <PageContainer>
+            <Header/>
+        </PageContainer>
+    );
 
     return (
         <PageContainer>
@@ -11,27 +29,25 @@ export default function HotelsDetailsPage() {
             <Content>
                 <h2>Hotel 1</h2>
                 <PicturesContainer>
-                    <img src="https://img.freepik.com/vetores-premium/fundo-de-edificio-de-hotel-flat_23-2148146118.jpg?w=2000" alt="" />
-                    <img src="https://img.freepik.com/vetores-premium/fundo-de-edificio-de-hotel-flat_23-2148146118.jpg?w=2000" alt="" />
-                    <img src="https://img.freepik.com/vetores-premium/fundo-de-edificio-de-hotel-flat_23-2148146118.jpg?w=2000" alt="" />
-                    <img src="https://img.freepik.com/vetores-premium/fundo-de-edificio-de-hotel-flat_23-2148146118.jpg?w=2000" alt="" />
-                    <img src="https://img.freepik.com/vetores-premium/fundo-de-edificio-de-hotel-flat_23-2148146118.jpg?w=2000" alt="" />
+                    {
+                    info.pictures 
+                    ? info.pictures.map((p, index) => <img key={index} src={p} alt=""/>)
+                    : ""
+                    }
                 </PicturesContainer>
                 <HotelContent>
                     <InfoContainer>
                         <h3>Características</h3>
                         <ul>
-                            <li>Local: Salvador</li>
-                            <li>Descrição: mslamls mlsmalms mmsmsmsmmla mmslamsmsm </li>
-                            <li>Preço: R$ 199.00</li>
+                            <li>Local: {cityName}</li>
+                            <li>Descrição: {info.description}</li>
+                            <li>Preço: R$ {(info.price_per_day/100).toFixed(2)}</li>
                         </ul>
                     </InfoContainer>
                     <AmenitiesContainer>
                         <h3>Comodidades</h3>
                         <ul>
-                            <li>Piscina</li>
-                            <li>Estacionamento</li>
-                            <li>Wifi</li>
+                            {info.amenities.map((a,index) => <li key={index}>{a}</li>)}
                         </ul>
                     </AmenitiesContainer>
                 </HotelContent>
